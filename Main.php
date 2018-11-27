6b097adb44c3e663b701325820b65f23c7817026
Marcelo Janke <marcelojanke@MacBook-Air-de-Marcelo.local>
2018-11-27 16:42:08 -0200

    primeiro commit

---
+++ Main.php
@@ -0,0 +1,24 @@
+<?php
+    include "Connect.php";
+
+    $classme = new Connection("Rogerio");
+
+    //$classme -> me();
+
+    $oi = "nao posso mae";
+    //$oi = str_split($oi);
+
+    echo $oi[1];     
+    $count = 0;
+    // foreach($oi as $i){
+    //     $count++;
+    //     if($count > 3){
+    //         echo $i . "\n";
+    //         $count = 0;
+    //     }else{
+    //         echo $i;
+    //     }
+
+    // }
+
+?>
