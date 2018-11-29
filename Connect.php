168754a57c83ce63ed40abafaca27a6a4af67919
MarceloJanke <marcelojanke@icloud.com>
2018-11-28 22:23:23 -0200

    php Test

--- Connect.php
+++ Connect.php
@@ -1,14 +1,9 @@
 <?php
     class Connection{
+        public $link, $name;
         
         function __construct($name){
-            echo "in class we trust " . $name . "\n";
-            $this -> link = new mysqli("127.0.0.1","root",
-                    "Valdisnei123","PJR_Desaparecidos"); 
-
-            if($this -> link -> connect_errno){
-                echo $this -> link -> connection_error;
-            }
+            $this-> name = $name;
         }
     
         public function me(){
