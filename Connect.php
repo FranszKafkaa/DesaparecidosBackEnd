c340dcdd9a0e95bfc623ac48e1dbb5f040a63748
MarceloJanke <marcelojanke@icloud.com>
2018-11-28 22:13:09 -0200

    link connection

--- Connect.php
+++ Connect.php
@@ -3,8 +3,13 @@
         
         function __construct($name){
             echo "in class we trust " . $name . "\n";
-            //$this -> link = new mysqli("127.0.0.1","root","Valdisnei123","PJR_Desaparecidos"); 
+            $this -> link = new mysqli("127.0.0.1","root","Valdisnei123","PJR_Desaparecidos"); 
+            
+            if($this -> link -> connect_errno){
+                echo $this -> link -> connection_error;
+            }
         }
+    
         public function me(){
             echo "Me " . $this -> name;
         }
