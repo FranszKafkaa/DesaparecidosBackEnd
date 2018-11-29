0ad314a432e0cae0c6243460d984c2413a083599
MarceloJanke <marcelojanke@icloud.com>
2018-11-28 23:40:29 -0200

    settings link mysqli

--- Connect.php
+++ Connect.php
@@ -3,11 +3,22 @@
         public $link, $name;
         
         function __construct($name){
-            $this-> name = $name;
+            $this -> name = $name;
+
+            $this -> link = new mysqli("h2cwrn74535xdazj.cbetxkdyhwsb.
+            us-east-1.rds.amazonaws.com","c7ogmojcci3nrmwp",
+            "usx7egblsam3bm7c","mr9tugmt9iix0e3z");
+
+            if($this-> link -> connect_error){
+                echo "nao main";
+            }else{
+                echo "Foi";
+            }
         }
     
-        public function me(){
+        public function Open(){
             echo "Me " . $this -> name;
+            
         }
     }
 ?>
