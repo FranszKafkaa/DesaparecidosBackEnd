f894ceb9f95c14b265ed0f1326ff33c7e89395ba
MarceloJanke <marcelojanke@icloud.com>
2018-11-28 22:25:45 -0200

    removing some crap

--- index.php
+++ index.php
@@ -4,26 +4,5 @@
     $classme = new Connection("Marcelo Janke");
 
     $classme -> me();
-
-    $oi = "nao posso mae";
-
-
     $out = shell_exec("uname");
-
-    //echo "<br>" . $out;
-    //$oi = str_split($oi);
-
-    echo $oi[1];     
-    $count = 0;
-    // foreach($oi as $i){
-    //     $count++;
-    //     if($count > 3){
-    //         echo $i . "\n";
-    //         $count = 0;
-    //     }else{
-    //         echo $i;
-    //     }
-
-    // }
-
 ?>
